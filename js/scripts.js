10ba9527343acec821bb52b5064e0cf43f9fbe14
leoheins <leo.heinsuo@gmail.com>
2023-09-03 21:44:49 +0300

    Fixed google maps (I think)

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,20 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Susanna and Leo's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
-
-            // Event duration (IN MINUTES)
-            // duration: 120,
+            start: new Date('Jun 4, 2023 17:00'),
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jun 10, 2023 10:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Agriturismo La Conca',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "Tervetuloa Leon ja Susannan häihin Toscanaan!"
         }
     });
 
@@ -255,7 +252,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 43.6052845, lng: 12.1430825};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
